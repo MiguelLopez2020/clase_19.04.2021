b3dece957a05b6a925ec01a013d0545840fcecc9
MiguelLopez2020 <miguel_angel_lopez_s@hotmail.com>
2021-04-20 10:18:20 -0500

    ejercicios 19 de abril del 2021 CICLO FOR

---
+++ code.js
@@ -0,0 +1,52 @@
+
+/*
+for (var x = 10; x >= 0; x--) {
+    console.log(x)
+}
+
+
+function suma() {
+
+    var uno = document.getElementById("numero1").value
+    var uno_ = parseInt(uno)
+    var dos = document.getElementById("numero2").value
+    var dos_ = parseInt(dos)
+    var suma = uno_ + dos_
+
+    var x = 0
+    for (var y = 1; y <= suma; y++) {
+
+        z = x + y
+        console.log(z)
+    }
+}
+
+
+function suma(){
+
+var x = document.getElementById("numero1").value
+
+if(x%2){
+    console.log ("impar")
+}else{
+    console.log("par")
+}
+
+}
+
+
+function suma() {
+
+    var x = document.getElementById("numero1").value
+
+    var y = document.getElementById("numero2").value
+
+
+    for (var a = 1; a <= y ; a++) {
+
+
+        console.log(x + "X" + a + "=" + (x*a))
+
+
+    }
+}*/
